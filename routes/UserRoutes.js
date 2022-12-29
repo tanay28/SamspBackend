7046b4b2bfd49f7dbf3f638ae8fcc53abba6a74b
Tanay <tmtanay56@gmail.com>
2022-12-29 20:16:01 +0530

    reorder route

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -4,8 +4,8 @@ const { validateOpenRequest } = require('../auth/request-validation');
 const { authoriseAdminRoutes } = require('../auth/authorise_admin_routes');
 const { checkToken } = require('../auth/Token_validation');
 
-router.post('/users', validateOpenRequest, userController.registerUser);
 router.get('/users', validateOpenRequest, authoriseAdminRoutes, userController.getAllUser);
+router.post('/users', validateOpenRequest, userController.registerUser);
 
 router.post('/users/forgotpassword', validateOpenRequest, userController.forgotPassword);
 router.post('/users/verifyuserotp', validateOpenRequest, userController.verifyUserOtp);
